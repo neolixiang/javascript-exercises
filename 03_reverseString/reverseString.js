b2c4046bb9594d43f64db6e5a5e102beacdd1524
neolixiang <22746477+neolixiang@users.noreply.github.com>
2022-11-09 23:21:04 -0500

    Reverse String
    Keep in mind that .split('') -
    if the quotes are missing, the string won't be split.
    The quote with nothing between them splits the string
    character by character.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(x) {
+    let myArray = x.split("");
+    let newString = '';
+    for (i = myArray.length - 1; i >= 0; i--) {
+        newString += `${myArray[i]}`;
+    }
+    return newString;
 };
 
 // Do not edit below this line
