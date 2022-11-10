7a04c3e2a8cba39de4f4dc100b228917192b8730
neolixiang <22746477+neolixiang@users.noreply.github.com>
2022-11-09 23:59:57 -0500

    .splice() method
    The splice() method changes the contents of an array by removing or
    replacing existing elements and/or adding new elements in place.
    The 2nd parameter indicates the number of characters to delete.
    The 3rd, 4th, 5th... parameters indicate the items to add to the array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function (myArray, a = '', b = '', c = '', d = '') {
+
+    const index_a = myArray.indexOf(a);
+    if (index_a > -1) {
+        myArray.splice(index_a, 1);
+    }
+    const index_b = myArray.indexOf(b);
+    if (index_b > -1) {
+        myArray.splice(index_b, 1);
+    }
+    const index_c = myArray.indexOf(c);
+    if (index_c > -1) {
+        myArray.splice(index_c, 1);
+    }
+    const index_d = myArray.indexOf(d);
+    if (index_d > -1) {
+        myArray.splice(index_d, 1);
+    }
+    return myArray;
 
 };
 
