d02577932916df1164c6609d6898feab08480100
neolixiang <22746477+neolixiang@users.noreply.github.com>
2022-11-09 23:03:08 -0500

    Repeat string practice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(x, y) {
+    let output = '';
+    for (let i = 0; i < y; i++) {
+        output += `${x}`;
+    }
+    if (y < 0) {
+        output += `ERROR`;
+    }
+    return output;
+    
 };
 
 // Do not edit below this line
