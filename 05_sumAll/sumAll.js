303e6ef5775bc6ddf03766017e27c1ba6d0e6448
neolixiang <22746477+neolixiang@users.noreply.github.com>
2022-11-10 00:32:35 -0500

    Number.isInteger(x) is a boolean

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+    if (Number.isInteger(x) && x >= 0 && Number.isInteger(y) && y >= 0) {
 
-};
+        if (x <= y) {
+            let sum = x;
+            for (let i = 1; i <= y - x; i++) {
+                sum += x + i;
+            }
+            return sum;
+        } else {
+            let sum = y;
+            for (let i = 1; i <= x - y; i++) {
+                sum += y + i;
+            }
+            return sum;
+        }
+    } else {
+        return 'ERROR';
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
