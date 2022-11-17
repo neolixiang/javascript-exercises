ffce72d9557dc0a03b6a3d3c3141080a907da190
neolixiang <22746477+neolixiang@users.noreply.github.com>
2022-11-17 03:27:53 -0500

    08 Calculator practice

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (x) {
+  if (x.length===0) {
+    return 0;
+  } else {
+  return x.reduce((a, b) => a + b)
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (x) {
+  return x.reduce((a, b) => a * b)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0) {
+    return 1;
+  } else {
+    let x = 1
+    for (let i = 1; i <= a; i++) {
+      x *= i
+    }
+    return x;
+  }
 };
 
 // Do not edit below this line
