27292c316f8beb5af7c552c45128188f6ab2bca6
neolixiang <22746477+neolixiang@users.noreply.github.com>
2022-11-18 00:47:56 -0500

    last excise!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,18 +1,34 @@
+// const findTheOldest = function(people) {
+//     return people.reduce((oldest, currentPerson) => {
+//         const oldestAge = getAge(oldest.yearOfDeath,oldest.yearOfBirth);
+//         const currentPersonAge = getAge(currentPerson.yearOfDeath,currentPerson.yearOfDeath);
+//         return oldestAge < currentPersonAge ? currentPersonAge : oldestAge;
+//     })
+// }
+
+// const getAge = function(a, b) {
+//     if (!a) {
+//         a = new Date().getFullYear();
+//     };
+//     return a - b;
+// }
+
+
+
 const findTheOldest = function(people) {
     return people.reduce((oldest, currentPerson) => {
-        const oldestAge = getAge(oldest.yearOfDeath,oldest.yearOfBirth);
-        const currentPersonAge = getAge(currentPerson.yearOfDeath,currentPerson.yearOfDeath);
-        oldestAge < currentPersonAge ? currentPersonAge : oldestAge;
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+      return oldestAge < currentAge ? currentPerson : oldest;
     })
-};
-
-const getAge = function(a, b) {
-    if (!a) {
-        let a = new Date().getFullYear();
-    };
-    return a - b;
-}
-
+  };
+  
+  const getAge = function(a, b) {
+    if (!b) {
+      b = new Date().getFullYear();
+    }
+    return b - a;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
